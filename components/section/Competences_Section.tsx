364e9ae052fadee1870db3a0ab3f4a46a75e3ef1
Mattéo ROSSI <designbymatrossi@gmail.com>
2023-10-02 22:48:27 +0200

    Fixing Responsive + List Logo

--- components/section/Competences_Section.tsx
+++ components/section/Competences_Section.tsx
@@ -2,21 +2,7 @@
 
 import Image from "next/image";
 import { motion } from "framer-motion";
-
-const logo = [
-  { img: '/logo langage/react_logo.png', alt: 'react_logo', width: 130, height: 130, className: 'absolute bottom-[23rem] left-[26rem]' },
-  { img: '/logo langage/figma.png', alt: 'figma_logo', width: 110, height: 110, className: 'absolute top-1/2 left-1/4' },
-  { img: '/logo langage/git.png', alt: 'git_logo', width: 60, height: 60, className: 'absolute top-1/4 right-1/4' },
-  { img: '/logo langage/nodejs.png', alt: 'node_logo', width: 65, height: 65, className: 'absolute top-10 right-1/4' },
-  { img: '/logo langage/php.png', alt: 'php_logo', width: 250, height: 250, className: 'absolute bottom-56 left-2/4' },
-  { img: '/logo langage/python.png', alt: 'python_logo', width: 50, height: 50, className: 'absolute bottom-1/4 left-16' },
-  { img: '/logo langage/mysql.png', alt: 'sql_logo', width: 70, height: 70, className: 'absolute bottom-44 left-1/3' },
-  { img: '/logo langage/strapi.png', alt: 'strapi_logo', width: 50, height: 50, className: 'absolute bottom-32 right-1/4 rounded-full' },
-  { img: '/logo langage/tailwind.png', alt: 'tailwind_logo', width: 80, height: 80, className: 'absolute top-16 left-1/3' },
-  { img: '/logo langage/typescript.png', alt: 'ts_logo', width: 40, height: 40, className: 'absolute top-1/2 right-5' },
-  { img: '/logo langage/vuejs.webp', alt: 'vuejs_logo', width: 50, height: 50, className: 'absolute bottom-3/4 left-24' },
-  { img: '/logo langage/Wordpress.webp', alt: 'wordpress_logo', width: 100, height: 100, className: 'absolute bottom-2/3 right-96' }
-]
+import { logo } from "../function/data";
 
 function Competences() {
   return (
@@ -53,7 +39,7 @@ function Competences() {
             }}
             src={'./logo langage/javascript.png'}
             alt="js_logo"
-            className="absolute top-28 left-36 w-50 h-40"
+            className="absolute xl:top-28 top-2 xl:left-36 left-12 lg:w-50 lg:h-40"
           />
           <motion.img
             initial={{ y: 50 }}
@@ -67,7 +53,7 @@ function Competences() {
             }}
             src={'./logo langage/github.webp'}
             alt="github_logo"
-            className="absolute lg:top-2/3 top-3/4 xl:right-56 right-24 w-28 h-28"
+            className="absolute lg:top-2/3 top-full xl:right-56 right-24 lg:w-28 w-20 h-20 lg:h-28"
           />
         </div>
       </section>
